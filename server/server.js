a98436d5eaea2b6d623bd993a4037f17971c0183
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-09-14 17:24:11 +0100

    Change .env file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const messageRoutes = require("./routes/messageRoutes");
@@ -8,6 +9,12 @@ connectDB();
 
 // start express server
 const app = express();
+
+// define which environment we are in
+const CORS_ORIGIN_DEV = process.env.CORS_ORIGIN_DEV || "http://localhost:5173";
+const CORS_ORIGIN_PROD = process.env.CORS_ORIGIN_PROD || "https://message-board-erinsophie.netlify.app";
+// set the origin based on that
+const currentOrigin = process.env.NODE_ENV === 'production' ? CORS_ORIGIN_PROD : CORS_ORIGIN_DEV;
 
 // middlewares
 app.use(cors({ origin: "http://localhost:5173" }));
