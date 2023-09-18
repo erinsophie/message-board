f2510a41de0efb50dc899a19ae033473ea01b83d
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-09-18 16:12:47 +0100

    Add production database

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+const dbURI =
+  process.env.NODE_ENV === "production"
+    ? process.env.PROD_DATABASE_URI
+    : process.env.DEV_DATABASE_URI;
+
 // connect to database
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@messageappcluster.yqgcukt.mongodb.net/MessagesDatabase?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database connected");
   } catch (err) {
     console.error(`Error connecting to database: ${err}`);
